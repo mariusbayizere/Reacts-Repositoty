1a54019f8c21fc7bad62d54bb3a388cd80e84407
Bayizere Marius <bayizeremarius119@gmail.com>
2024-11-13 11:05:52 +0200

    this commit to git

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+
+const Hello = () => {
+   
+   return (<h1>Hello my peaple A'm Here to tolk about Reacts </h1>)
+}
+
+
+
+const Heading = () => {
+
+    return (
+        <div>
+<Hello></Hello>
+    {7839}
+<h1>This React components using stateless functinal components</h1>
+</div>
+);
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Heading></Heading>)
